6ac1e09438952c9fd7896a1a7aad50fc7925316d
Shiropan <loliolev@proton.me>
2025-02-20 02:58:33 -0300

    add a local build script

---
+++ build.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+set -e
+h_open_f="\e[1;34m:: \e[1;97m"
+h_open="\n\e[1;34m:: \e[1;97m"
+h_close="\e[0m"
+
+echo -e "${h_open_f}Pulling submodules...${h_close}"
+git pull --recurse-submodules
+
+echo -e "${h_open}Setting up Python environment...${h_close}"
+cd unpack
+pdm install
+source .venv/bin/activate
+pip install -r downloader/requirements.txt
+
+echo -e "${h_open}Downloading resources...${h_close}"
+mkdir -p downloader/resdata
+cp gf-data-us/resdata_no_hash.json downloader/resdata/us_resdata.json
+cd downloader
+sed -i 's/"ch"/"us"/' config.json5
+git apply ../downloader.patch || true
+zip -j resdata.zip resdata/us_resdata.json
+python downloader.py --downloadres 0 --abname
+
+echo -e "${h_open}Unpacking images...${h_close}"
+cd ..
+cp -r fixed-data/* gf-data-us
+python tests/test_backgrounds.py
+python tests/test_characters.py
+
+echo -e "${h_open}Unpacking and converting audio files...${h_close}"
+python tests/test_audio.py
+
+echo -e "${h_open}Processing resources...${h_close}"
+python -m gfunpack downloader/output -o .
+
+echo -e "${h_open}Moving resources...${h_close}"
+cp audio/audio.json images/backgrounds.json images/characters.json stories/stories.json stories/chapters.json ../src/assets
+if [[ -d "../public/audio" ]]; then
+  rm -rf ../public/audio
+fi
+mv audio ../public
+if [[ -d "../public/images" ]]; then
+  rm -rf ../public/images
+fi
+mv images ../public
+if [[ -d "../public/stories" ]]; then
+  rm -rf ../public/stories
+fi
+mv stories ../public/stories
+
+echo -e "${h_open}Building site...${h_close}"
+cd ..
+pnpm install
+pnpm build-index
+pnpm build
+
+echo -e "\n\e[1;42mBUILD COMPLETE!\e[0m"
+echo -e "\e[1;97mYour site is available at \e[1;33m$PWD/dist/\e[0m"
